3b046e242d01845918a97989e19329fcad6568fc
Zunnoorain Rafi <zunnoorainrafi236@gmail.com>
2024-07-04 18:52:02 +0500

    backend is initialized

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'AOA Zunnoorain!' });
+});
+
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
